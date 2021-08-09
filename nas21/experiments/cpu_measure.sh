5f8360a1b0d3b04650379420192ee1e89351f773
sqi009 <sqi009@node0.gp-01.kkprojects-pg0.utah.cloudlab.us>
2021-08-09 16:45:25 -0600

    add cpu measurement script

---
+++ nas21/experiments/cpu_measure.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "========= Check whether mpstat is installed ========="
+if command -v mpstat >/dev/null 2>&1; then
+    echo 'mpstat installed'
+else
+    echo 'Please install mpstat: sudo apt install sysstat'
+    exit 1
+fi
+
+echo "========= Check whether bc is installed ========="
+if command -v bc >/dev/null 2>&1; then
+    echo 'bc installed'
+else
+    echo 'Please install bc: sudo apt-get install -y bc'
+    exit 1
+fi
+
+# Run mpstat to collect CPU percentage
+mpstat_result=$(mpstat 1 1 | tail -1)
+# Setting space as spliter 
+IFS=' '
+# Reading mpstat_result as an array as tokens separated by IFS
+read -ra cpu_usage <<<"$mpstat_result" 
+# echo "${cpu_usage[-1]}"
+cpu_upper_limit=100
+cpu_idle=$(echo ${cpu_usage[-1]} | bc -l)
+total_cpu=$(echo "$cpu_upper_limit - $cpu_idle" | bc)
+echo "CPU usage: $total_cpu"
